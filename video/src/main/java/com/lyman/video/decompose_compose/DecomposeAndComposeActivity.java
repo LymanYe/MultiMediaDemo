22a51695aa33ec7c85e21e836d2269ab1808557e
叶立民 <lymanye@gmail.com>
2018-01-11 14:14:17 +0800

    update compose mp4 file sample

--- video/src/main/java/com/lyman/video/decompose_compose/DecomposeAndComposeActivity.java
+++ video/src/main/java/com/lyman/video/decompose_compose/DecomposeAndComposeActivity.java
@@ -21,6 +21,7 @@ import java.io.File;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class DecomposeAndComposeActivity extends AppCompatActivity {
     private static final String TAG = "DecomposeAndComposeActivity";
 
@@ -28,19 +29,23 @@ public class DecomposeAndComposeActivity extends AppCompatActivity {
     private Handler mHandler;
     private File outputAudioFile;
     private File outputVideoFile;
+    private File inputVideoFile;
     private ProgressBar mProgressBar;
     private TextView mAudioPathTV;
     private TextView mVideoPathTV;
+    private TextView mCombineVideoPathTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decompose_and_compose);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        outputAudioFile = getOutputMediaFile(MEDIA_TYPE_AUDIO);
-        outputVideoFile = getOutputMediaFile(MEDIA_TYPE_VIDEO);
+        outputAudioFile = getOutputMediaFile(MEDIA_TYPE_AUDIO, "AUDIO_OUTPUT_DIVIDE");
+        outputVideoFile = getOutputMediaFile(MEDIA_TYPE_VIDEO, "MP4_OUTPUT_DIVIDE");
+        inputVideoFile = getOutputMediaFile(MEDIA_TYPE_VIDEO, "MP4_INPUT_COMBINE");
         mAudioPathTV = (TextView) findViewById(R.id.audio_path_tv);
         mVideoPathTV = (TextView) findViewById(R.id.video_path_tv);
+        mCombineVideoPathTV = (TextView) findViewById(R.id.combine_video_path_tv);
     }
 
     @Override
@@ -51,7 +56,6 @@ public class DecomposeAndComposeActivity extends AppCompatActivity {
         mHandler = new Handler(mHandlerThread.getLooper());
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onPause() {
         super.onPause();
@@ -65,7 +69,36 @@ public class DecomposeAndComposeActivity extends AppCompatActivity {
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void combineVideo(View view) {
+        if (inputVideoFile.exists()) {
+            Toast.makeText(this, "file exit", Toast.LENGTH_SHORT).show();
+            mCombineVideoPathTV.setText("Video Path:" + inputVideoFile.getAbsolutePath());
+            return;
+        }
+
+        if (!outputAudioFile.exists() || !outputVideoFile.exists()
+                || outputAudioFile.length() < 0 || outputVideoFile.length() < 0) {
+            Toast.makeText(this, "decompose video first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mProgressBar.setVisibility(View.VISIBLE);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                MediaUtil.getInstance().combineVideo(outputVideoFile,
+                        outputAudioFile, inputVideoFile);
+                DecomposeAndComposeActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mProgressBar.setVisibility(View.GONE);
+                        mCombineVideoPathTV.setText("Video Path:" + inputVideoFile.getAbsolutePath());
+                    }
+                });
+            }
+        });
+    }
+
+
     public void decomposeVideo(View view) {
         if (outputAudioFile.exists() && outputVideoFile.exists()
                 && outputAudioFile.length() > 0 && outputVideoFile.length() > 0) {
@@ -92,14 +125,14 @@ public class DecomposeAndComposeActivity extends AppCompatActivity {
         });
     }
 
-    private File getOutputMediaFile(int mediaType) {
+    private File getOutputMediaFile(int mediaType, String name) {
         String fileName = null;
         File storageDir = null;
         if (mediaType == MEDIA_TYPE_AUDIO) {
-            fileName = "AUDIO_OUTPUT_DIVIDE" + "_";
+            fileName = name;
             storageDir = getExternalFilesDir(Environment.DIRECTORY_PODCASTS);
         } else if (mediaType == MEDIA_TYPE_VIDEO) {
-            fileName = "MP4_OUTPUT_DIVIDE" + "_";
+            fileName = name;
             storageDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES);
         }
 
--- video/src/main/java/com/lyman/video/decompose_compose/utils/MediaUtil.java
+++ video/src/main/java/com/lyman/video/decompose_compose/utils/MediaUtil.java
@@ -24,6 +24,7 @@ public class MediaUtil {
     private static final String TAG = "MediaUtil";
     public final static String AUDIO_MIME = "audio";
     public final static String VIDEO_MIME = "video";
+    private final static int ALLOCATE_BUFFER = 500 * 1024;
     private static MediaUtil mInstance;
 
     private MediaUtil() {
@@ -136,6 +137,8 @@ public class MediaUtil {
         Log.d(TAG, "rate:" + sampleRate + ",c:" + channelCount);
         //获取音频总时长
         long audioDuration = format.getLong(MediaFormat.KEY_DURATION);
+
+
         //参数为多媒体文件MediaExtractor获取到的track count的索引,选择音频轨道
         mediaExtractor.selectTrack(audioTrackIndex);
 
@@ -182,5 +185,172 @@ public class MediaUtil {
         }
         //取消MediaExtractor选择当前轨道
         mediaExtractor.unselectTrack(audioTrackIndex);
+    }
+
+    public void combineVideo(File inputVideoFile, File inputAudioFile, File outputVideoFile) {
+        MediaExtractor videoExtractor = new MediaExtractor();
+        MediaExtractor audioExtractor = new MediaExtractor();
+        MediaMuxer mediaMuxer = null;
+        try {
+            mediaMuxer = new MediaMuxer(outputVideoFile.getAbsolutePath(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+            // set data source
+            videoExtractor.setDataSource(inputVideoFile.getAbsolutePath());
+            audioExtractor.setDataSource(inputAudioFile.getAbsolutePath());
+
+            // get video or audio 取出视频或音频的信号
+            int videoTrack = getTrack(videoExtractor, true);
+            int audioTrack = getTrack(audioExtractor, false);
+
+            // change to video oraudio track 切换道视频或音频信号的信道
+            videoExtractor.selectTrack(videoTrack);
+            MediaFormat videoFormat = videoExtractor.getTrackFormat(videoTrack);
+            audioExtractor.selectTrack(audioTrack);
+            MediaFormat audioFormat = audioExtractor.getTrackFormat(audioTrack);
+
+            //追踪此信道
+            int writeVideoIndex = mediaMuxer.addTrack(videoFormat);
+            int writeAudioIndex = mediaMuxer.addTrack(audioFormat);
+            mediaMuxer.start();
+
+            // 读取写入帧数据
+            writeSampleData(videoExtractor, mediaMuxer, writeVideoIndex, videoTrack);
+            writeSampleData(audioExtractor, mediaMuxer, writeAudioIndex, audioTrack);
+        } catch (IOException e) {
+            Log.w(TAG, "combineMedia ex", e);
+        } finally {
+            try {
+                if (mediaMuxer != null) {
+                    mediaMuxer.stop();
+                    mediaMuxer.release();
+                }
+                if (videoExtractor != null) {
+                    videoExtractor.release();
+                }
+                if (audioExtractor != null) {
+                    audioExtractor.release();
+                }
+            } catch (Exception e) {
+                Log.w(TAG, "combineMedia release ex", e);
+            }
+        }
+    }
+
+    /**
+     * write sample data to mediaMuxer
+     *
+     * @param mediaExtractor
+     * @param mediaMuxer
+     * @param writeTrackIndex
+     * @param audioTrack
+     * @return
+     */
+    private boolean writeSampleData(MediaExtractor mediaExtractor, MediaMuxer mediaMuxer,
+                                    int writeTrackIndex, int audioTrack) {
+        try {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(ALLOCATE_BUFFER);
+
+            // 读取写入帧数据
+            long sampleTime = getSampleTime(mediaExtractor, byteBuffer, audioTrack);
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+
+            while (true) {
+                //读取帧之间的数据
+                int readSampleSize = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleSize < 0) {
+                    break;
+                }
+
+                mediaExtractor.advance();
+                bufferInfo.size = readSampleSize;
+                bufferInfo.offset = 0;
+                bufferInfo.flags = mediaExtractor.getSampleFlags();
+                bufferInfo.presentationTimeUs += sampleTime;
+                //写入帧的数据
+                mediaMuxer.writeSampleData(writeTrackIndex, byteBuffer, bufferInfo);
+            }
+            return true;
+        } catch (Exception e) {
+            Log.w(TAG, "writeSampleData ex", e);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param mediaExtractor
+     * @param isMedia        true: get "video/"
+     *                       false get "audio/"
+     * @return
+     */
+    private int getTrack(MediaExtractor mediaExtractor, boolean isMedia) {
+        if (mediaExtractor == null) {
+            Log.w(TAG, "mediaExtractor mediaExtractor is null");
+            return 0;
+        }
+        String type = isMedia ? "video/" : "audio/";
+        int trackCount = mediaExtractor.getTrackCount();
+        for (int i = 0; i < trackCount; i++) {
+            MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+            String mineType = trackFormat.getString(MediaFormat.KEY_MIME);
+            // video or audio track
+            if (mineType.startsWith(type)) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * 获取每帧的之间的时间
+     *
+     * @return
+     */
+    private long getSampleTime(MediaExtractor mediaExtractor, ByteBuffer byteBuffer, int videoTrack) {
+        if (mediaExtractor == null) {
+            Log.w(TAG, "getSampleTime mediaExtractor is null");
+            return 0;
+        }
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        //skip first I frame
+        if (mediaExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC) {
+            mediaExtractor.advance();
+        }
+        mediaExtractor.readSampleData(byteBuffer, 0);
+
+        // get first and second and count sample time
+        long firstVideoPTS = mediaExtractor.getSampleTime();
+        mediaExtractor.advance();
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        long SecondVideoPTS = mediaExtractor.getSampleTime();
+        long sampleTime = Math.abs(SecondVideoPTS - firstVideoPTS);
+        Log.d(TAG, "getSampleTime is " + sampleTime);
+
+        // 重新切换此信道，不然上面跳过了3帧,造成前面的帧数模糊
+        mediaExtractor.unselectTrack(videoTrack);
+        mediaExtractor.selectTrack(videoTrack);
+
+        return sampleTime;
+    }
+
+    private int prepareMediaInfo(MediaExtractor mediaExtractor, MediaMuxer mediaMuxer,
+                                 int mediaTrack, boolean isMedia) {
+        try {
+            // change to video oraudio track 切换道视频或音频信号的信道
+            mediaExtractor.selectTrack(mediaTrack);
+            MediaFormat trackFormat = mediaExtractor.getTrackFormat(mediaTrack);
+
+            //追踪此信道
+            int writeTrackIndex = mediaMuxer.addTrack(trackFormat);
+            mediaMuxer.start();
+
+            return writeTrackIndex;
+
+        } catch (Exception e) {
+            Log.w(TAG, "prepareMediaInfo ex", e);
+        }
+
+        return 0;
     }
 }
